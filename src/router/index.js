2393f9904de77fd418744902d77948207ff1bd10
Anton K <mi3ta@sent.as>
2020-02-17 11:02:11 +0700

    :fire: Removed unusual views/routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,17 +5,7 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import(/* webpackChunkName: "about" */ '@/views/Home.vue')
-  },
-  {
-    path: '/search',
-    name: 'Search',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/views/Search.vue')
+    path: '/'
   }
 ]
 
