a50e6d1569508d3b432dcbed24086571b811a644
Anton K <mi3ta@sent.as>
2020-02-17 11:35:26 +0700

    :alien: Connected app to API

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,68 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/search">Search</router-link>
-    </div>
-    <router-view/>
+    <search :q="q" />
+    <template v-for="(user, index) in filtered">
+      <card :key="index" :user="user" :q="q" />
+    </template>
   </div>
 </template>
 
+<script>
+import Search from '@/components/Search'
+import Card from '@/components/Card'
+export default {
+  components: {
+    Search,
+    Card
+  },
+  data: () => ({
+    users: [],
+    error: null
+  }),
+  computed: {
+    q () {
+      return this.$route.path.split('/')[2] || ''
+    },
+    filtered () {
+      const re = new RegExp(this.q, 'ig')
+      return this.users.filter(u => Object.values(u).join().match(re))
+    }
+  },
+  async created () {
+    try {
+      const res = await fetch('/api/users')
+      this.users = await res.json()
+    } catch (err) {
+      this.error = err
+    }
+  }
+}
+</script>
+
 <style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background: #EEEEEE;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+}
+
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: rgba(0, 0, 0, 0.87);
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  width: 564px;
+  height: 643px;
+  overflow: auto;
+  padding: 19px 12px 0;
+  background-color: #FFFFFF;
 }
 </style>
